2922b9d31822c854cb129156332e1232d614909e
pushyahass <31552646+pushyahass@users.noreply.github.com>
2018-07-03 14:09:08 -0500

    commit9

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def traffic():
     # url = "http://quiz8-env.3ftfvmwv4e.us-east-2.elasticbeanstalk.com/"
     for i in range(0, 1500):
         # subprocess.urllib.urlopen("http://quiz8-env.3ftfvmwv4e.us-east-2.elasticbeanstalk.com/")
-        x = urllib.request.urlopen("http://quiz8-env.3ftfvmwv4e.us-east-2.elasticbeanstalk.com/")
+        urllib.request.urlopen("http://18.191.147.241/show")
         # subprocess.Popen("http://quiz8-env.3ftfvmwv4e.us-east-2.elasticbeanstalk.com/", shell=True)
         # subprocess.Popen(['python3', '-u', '-m', "http://quiz8-env.3ftfvmwv4e.us-east-2.elasticbeanstalk.com/"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     return render_template("traffic.html")
